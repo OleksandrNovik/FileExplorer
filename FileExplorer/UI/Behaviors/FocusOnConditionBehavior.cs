6354f8fbcf148e3d10a01f089864f11f1e5fe0d5
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-07-02 12:15:41 +0300

    Separated logic "Lost focus"  "Focus+select all text" logic to a different behaviors, so now they have more re-usability and can be used independently from each other

---
+++ FileExplorer/UI/Behaviors/FocusOnConditionBehavior.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+
+namespace FileExplorer.UI.Behaviors
+{
+    public class FocusOnConditionBehavior : Behavior<TextBox>
+    {
+        public bool Condition
+        {
+            get => (bool)GetValue(ConditionProperty);
+            set => SetValue(ConditionProperty, value);
+
+        }
+
+        public static readonly DependencyProperty ConditionProperty =
+            DependencyProperty.Register(nameof(Condition), typeof(bool),
+                typeof(FocusOnConditionBehavior), new PropertyMetadata(false, OnConditionChanged));
+
+        private static void OnConditionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FocusOnConditionBehavior behavior)
+            {
+                if ((bool)e.NewValue)
+                {
+                    behavior.AssociatedObject.Focus(FocusState.Programmatic);
+                    behavior.AssociatedObject.SelectAll();
+                }
+            }
+        }
+    }
+}
---
+++ FileExplorer/UI/Behaviors/LostFocusBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace FileExplorer.UI.Behaviors
+{
+    public class LostFocusBehavior : BaseCommandBehavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.LostFocus += OnLostFocus;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.LostFocus -= OnLostFocus;
+        }
+
+        private void OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Command.CanExecute(CommandParameter))
+            {
+                Command.Execute(CommandParameter);
+            }
+        }
+    }
+}
