52bb6c7b7d588ac1b8603b9578fe33eb4def721f
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-09-12 14:42:57 +0300

    Created formatter to format bytes number into more readable string

---
+++ FileExplorer/UI/Converters/NumberFormatter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace FileExplorer.UI.Converters
+{
+    /// <summary>
+    /// Formats provided long number to a "123123 = 123 123" format
+    /// </summary>
+    public sealed class NumberFormatter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string result = string.Empty;
+
+            if (value is long number)
+            {
+                result = $"{number:N0}";
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
